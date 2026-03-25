0e659b1b3a527b7d41231e4e8a935eb7c48e5f65
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-03-25 18:01:59 +0100

    tweaked shit although currently broken

--- qmk_firmware/keyboards/penor/config.h
+++ qmk_firmware/keyboards/penor/config.h
@@ -19,9 +19,7 @@
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
 
-// #define MASTER_LEFT
-#define MASTER_RIGHT
-// #define EE_HANDS
+#define SPLIT_HAND_PIN GP29
 
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 175
@@ -32,6 +30,6 @@
 #define MOUSEKEY_TIME_TO_MAX    80
 
 // stuff for Tri Layers
-#define TRI_LAYER_LOWER_LAYER   4
-#define TRI_LAYER_UPPER_LAYER   5
-#define TRI_LAYER_ADJUST_LAYER  6
+#define TRI_LAYER_LOWER_LAYER   2
+#define TRI_LAYER_UPPER_LAYER   3
+#define TRI_LAYER_ADJUST_LAYER  4
